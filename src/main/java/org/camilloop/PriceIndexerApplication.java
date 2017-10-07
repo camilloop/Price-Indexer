36d08e5d9b825030985a02ade7c9aef4a7f519da
camilloop <kamilpaluch@gmail.com>
2017-10-07 18:24:07 +0200

    git init

---
+++ src/main/java/org/camilloop/PriceIndexerApplication.java
@@ -0,0 +1,12 @@
+package org.camilloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceIndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceIndexerApplication.class, args);
+	}
+}
