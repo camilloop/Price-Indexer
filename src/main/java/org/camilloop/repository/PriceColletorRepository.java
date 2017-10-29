d291b2ec536fdc6b3862849a02436912e836f57b
camilloop <kamilpaluch@gmail.com>
2017-10-29 12:34:34 +0100

    repositories methods for price collector

--- src/main/java/org/camilloop/repository/PriceColletorRepository.java
+++ src/main/java/org/camilloop/repository/PriceColletorRepository.java
@@ -1,7 +1,23 @@
 package org.camilloop.repository;
 
 import org.camilloop.model.PriceCollector;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
 public interface PriceColletorRepository extends ElasticsearchRepository<PriceCollector, String> {
+    List<PriceCollector> findAllByProductName(String productName);
+    List<PriceCollector> findAllByWebsiteName(String productName);
+    List<PriceCollector> findByProductName(String productName, Pageable pageable);
+
+    /**
+     * findFist and FindTop don't work with elasticsearch
+     */
+    default List<PriceCollector> findFirstByProductNameOrderByPriceAsc(String productName) {
+        Sort sort = new Sort(Sort.Direction.ASC, "price");
+        return this.findByProductName(productName, PageRequest.of(0, 1, sort));
+    }
 }
