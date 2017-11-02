5d38783a0288dc51292bbad2d0d052ff8acbed89
camilloop <kamilpaluch@gmail.com>
2017-11-02 21:33:35 +0100

    adding fields createdAt and productUrl in PriceCollector model

--- src/main/java/org/camilloop/model/PriceCollector.java
+++ src/main/java/org/camilloop/model/PriceCollector.java
@@ -2,6 +2,7 @@ package org.camilloop.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import java.sql.Timestamp;
 
 @Document(indexName = "price_indexer", type = "price_collector", shards = 1, replicas = 0, refreshInterval = "-1")
 public class PriceCollector {
@@ -11,17 +12,23 @@ public class PriceCollector {
 
     private String websiteName;
 
+    private String productUrl;
+
     private String productName;
 
     private Double price;
 
+    private Timestamp createdAt;
+
     public PriceCollector() {
     }
 
-    public PriceCollector(String websiteName, String productName, Double price) {
+    public PriceCollector(String websiteName, String productUrl, String productName, Double price) {
         this.websiteName = websiteName;
+        this.productUrl = productUrl;
         this.productName = productName;
         this.price = price;
+        this.createdAt = new Timestamp(System.currentTimeMillis());
     }
 
     public String getId() {
@@ -40,6 +47,14 @@ public class PriceCollector {
         this.websiteName = websiteName;
     }
 
+    public String getProductUrl() {
+        return productUrl;
+    }
+
+    public void setProductUrl(String productUrl) {
+        this.productUrl = productUrl;
+    }
+
     public String getProductName() {
         return productName;
     }
@@ -56,13 +71,23 @@ public class PriceCollector {
         this.price = price;
     }
 
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
     @Override
     public String toString() {
         return "PriceCollector{" +
                 "id='" + id + '\'' +
                 ", websiteName='" + websiteName + '\'' +
+                ", productUrl='" + productUrl + '\'' +
                 ", productName='" + productName + '\'' +
                 ", price=" + price +
+                ", createdAt=" + createdAt +
                 '}';
     }
 }
