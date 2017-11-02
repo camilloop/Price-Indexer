b61e49337df813c14926023cc4f02e5ed5dafcdf
camilloop <kamilpaluch@gmail.com>
2017-11-02 21:32:11 +0100

    adding new field to website model for jsoup

--- src/main/java/org/camilloop/model/Website.java
+++ src/main/java/org/camilloop/model/Website.java
@@ -9,11 +9,20 @@ public class Website {
     @Id
     private String name;
 
+    private String namePattern;
+
+    private String pricePattern;
+
+    private int maxDepth;
+
     public Website() {
     }
 
-    public Website(String name) {
+    public Website(String name, String namePattern, String pricePattern, int maxDepth) {
         this.name = name;
+        this.namePattern = namePattern;
+        this.pricePattern = pricePattern;
+        this.maxDepth = maxDepth;
     }
 
     public String getName() {
@@ -24,10 +33,37 @@ public class Website {
         this.name = name;
     }
 
+    public String getNamePattern() {
+        return namePattern;
+    }
+
+    public void setNamePattern(String namePattern) {
+        this.namePattern = namePattern;
+    }
+
+    public String getPricePattern() {
+        return pricePattern;
+    }
+
+    public void setPricePattern(String pricePattern) {
+        this.pricePattern = pricePattern;
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+
+    public void setMaxDepth(int maxDepth) {
+        this.maxDepth = maxDepth;
+    }
+
     @Override
     public String toString() {
         return "Website{" +
                 "name='" + name + '\'' +
+                ", namePattern='" + namePattern + '\'' +
+                ", pricePattern='" + pricePattern + '\'' +
+                ", maxDepth=" + maxDepth +
                 '}';
     }
 }
