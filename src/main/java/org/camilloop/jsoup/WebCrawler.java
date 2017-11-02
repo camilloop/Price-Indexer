00bf94b3681a3150824b668eee7e57bec29be8d3
camilloop <kamilpaluch@gmail.com>
2017-11-02 21:37:17 +0100

    adding web crawler to get all link from website

---
+++ src/main/java/org/camilloop/jsoup/WebCrawler.java
@@ -0,0 +1,52 @@
+package org.camilloop.jsoup;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+public class WebCrawler {
+    private String url;
+    private int maxDepth;
+    private HashSet<String> links;
+
+    public WebCrawler(String url, int maxDepth) {
+        this.url = url;
+        this.maxDepth = maxDepth;
+        this.links = new HashSet<>();
+    }
+
+    public HashSet<String> getPageLinks() {
+        this.getPageLinks(url, 0);
+        return this.links;
+    }
+
+    private void getPageLinks(String url, int depth) {
+        if (!this.links.contains(url) && depth <= this.maxDepth) {
+            //System.out.println(">> Depth: " + depth + " [" + url + "]");
+            try {
+                this.links.add(url);
+
+                Document document = Jsoup.connect(url).get();
+                Elements linksOnPage = document.select("a[href^=\"" + url + "\"]");
+
+                depth++;
+                for (Element page : linksOnPage) {
+                    this.getPageLinks(page.attr("abs:href"), depth);
+                }
+            } catch (IOException e) {
+                System.err.println("For '" + url + "': " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "WebCrawler{" +
+                "links=" + links +
+                '}';
+    }
+}
